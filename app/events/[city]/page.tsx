fc97e2a1442b73dfbaf9085312654875d4d197e5
Elif Varli <elifyilbasi@gmail.com>
2026-02-13 15:33:24 +0100

    add events and event item related components

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,3 +1,28 @@
-export default function EventsPage() {
-  return <div>Events Page</div>;
+import EventsList from "@/components/events-list";
+import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
+
+type EventsPageProps = {
+  params: Promise<{ city: string }>;
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const { city } = await params;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=austin`,
+  );
+
+  const events: EventoEvent[] = await response.json();
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20x] min-h-[110vh]">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events In ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+      <EventsList events={events} />
+    </main>
+  );
 }
